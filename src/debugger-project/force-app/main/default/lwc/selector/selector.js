3693341b3a59b6c5586ad7acb556f2c847707a77
Matheus Falcirolli <fmatheus8@gmail.com>
2024-05-14 22:41:20 -0300

    T9

--- src/debugger-project/force-app/main/default/lwc/selector/selector.js
+++ src/debugger-project/force-app/main/default/lwc/selector/selector.js
@@ -4,6 +4,9 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
 import NAME_FIELD from '@salesforce/schema/User.Name';
 
+import FahrenheitToCelsius from '@salesforce/apex/TemperatureConverter.FahrenheitToCelsius'
+import ObterPrimeiraConta from '@salesforce/apex/AccountTriggerHandler.ObterPrimeiraConta';
+
 const fields = [NAME_FIELD];
 
 export default class Selector extends LightningElement {
@@ -18,5 +21,17 @@ export default class Selector extends LightningElement {
 
     get name(){
         return getFieldValue(this.user.data, NAME_FIELD);
+    }
+
+    @wire(FahrenheitToCelsius, {fh: 232.76}) temp;
+
+    get temperatura(){
+        return this.temp.data;
+    }
+
+    @wire(ObterPrimeiraConta, {billingState: 'CA'}) account;
+    
+    get conta(){
+        return this.account.data;
     }
 }
